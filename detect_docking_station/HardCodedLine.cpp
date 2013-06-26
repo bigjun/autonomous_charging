1ee1769bcbd10ac4920063affd72087e1f99768a
johane <johane@johane.(none)>
2013-06-26 12:29:49 +0200

    changed hardcoded line files to have starts/stops

--- detect_docking_station/HardCodedLine.cpp
+++ detect_docking_station/HardCodedLine.cpp
@@ -4,15 +4,15 @@ int HardCodedLine_counter = 0;
 HardCodedLine::HardCodedLine(float x1, float y1, float x2, float y2)
 {
 	id = HardCodedLine_counter++;
-	dir_x = x1-x2;
-	dir_y = y2-y2;
-	point_x = x1;
-	point_y = y1;
-	//TODO
-	transformed_dir_x = 0;
-	transformed_dir_y = 0;
-	transformed_point_x = 0;
-	transformed_point_y = 0;
+	point_x1 = x1;
+	point_Y1 = y1;
+	point_x2 = x2;
+	point_y2 = y2;
+
+	transformed_point_x1 = x1;
+	transformed_point_y1 = y1;
+	transformed_point_x2 = x2;
+	transformed_point_y2 = y2;
 }
 
 HardCodedLine::HardCodedLine(){id = HardCodedLine_counter++;}
--- detect_docking_station/HardCodedLine.h
+++ detect_docking_station/HardCodedLine.h
@@ -5,20 +5,31 @@ class HardCodedLine
 	public:
 	int id;
 
-	float dir_x;
-	float dir_y;
-	float point_x;
-	float point_y;
+	//Base parameters of the line
+	float point_x1;
+	float point_Y1;
+	float point_x2;
+	float point_y2;
 
-	float transformed_dir_x;
-	float transformed_dir_y;
-	float transformed_point_x;
-	float transformed_point_y;
+	//Transformed the base-points of the line, this will be used to test different transformations of the line.
+	float transformed_point_x1;
+	float transformed_point_y1;
+	float transformed_point_x2;
+	float transformed_point_y2;
 	
+	//Input the start and the stop of the line
 	HardCodedLine(float x1, float y1, float x2, float y2);	
+
+	//Standard constructor, does nothing exept give the segment an id.	
 	HardCodedLine();
+
+	//Standard destructor
 	~HardCodedLine();
+
+	//Transforms the points of the line so that we can test different transformations of models.
 	void transform(float tx, float ty, float r);
+
+	//Distance to line between transformed points
 	float distance(float x, float y);
 };
 #endif
