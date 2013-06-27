49a752666e6924a401a165a6a7fd71db8e6ae95c
johane <johane@johane.(none)>
2013-06-27 17:09:21 +0200

    Added some documentation and cleaned the DockingStationFinderSegmented files

--- detect_docking_station/src/DockingStationFinderSegmented.h
+++ detect_docking_station/src/DockingStationFinderSegmented.h
@@ -11,14 +11,17 @@ using namespace std;
 class DockingStationFinderSegmented : public DockingStationFinder
 {
 	public:
+	float max_angle_diff;//max dot product between slopes(if larger than this then segment)
+	float min_segment_size;//The minimum distance between the first and the last point of the segment
+	float max_segment_size;//The maximum distance between the first and the last point of the segment
+	float max_diff_from_expected;//Makes sure there is a certain difference between length of segment and distance between first and last point
+	float expected_diff;//Expected difference between length of segment and distance between first and last point
+
 	DockingStationFinderSegmented();
 	~DockingStationFinderSegmented();
 
+	//Finds likely corner of the chargeing station
 	vector<float> getMostLikelyLocation(vector<float> & laser_x, vector<float> & laser_y);
-	private:
-	vector<HardCodedLine*> model;
-	//float score(vector<HardCodedLine*> & model, vector<float> & x, vector<float> & y, float noise);
-
 };
 
 #endif
